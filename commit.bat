9aaee14e931f440fa9e0a1a5940e751ad2b0455c
MCoded <34777184+RealMCoded@users.noreply.github.com>
2021-06-11 13:21:28 -0400

    Delete commit.bat

--- commit.bat
+++
@@ -1,7 +0,0 @@
-echo "# Layerformer_10-06-20" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RealMCoded/Layerformer_10-06-20.git
-git push -u origin main
